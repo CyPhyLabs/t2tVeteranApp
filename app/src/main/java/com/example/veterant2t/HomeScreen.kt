4329196746f353209018c639a44f8d5830a19314
Vegito2367 <tejaaditya.g@gmail.com>
2024-08-12 15:32:56 +0400

    Initial Commit

---
+++ app/src/main/java/com/example/veterant2t/HomeScreen.kt
@@ -0,0 +1,51 @@
+package com.example.veterant2t
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.veterant2t.databinding.ActivityHomeScreenBinding
+
+class HomeScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_home->{
+                    updateFragment(HomeFragment())
+
+                }
+                R.id.navigation_dashboard->{
+                    updateFragment(DashboardFragment())
+
+                }
+                R.id.navigation_notifications->{
+                    updateFragment(NotifFragment())
+
+                }
+                else->{
+
+                }
+            }
+            true
+        }
+    }
+
+    fun updateFragment(fragment: Fragment){
+        val fragManager=supportFragmentManager
+        val fragTransaction=fragManager.beginTransaction()
+        fragTransaction.replace(R.id.fragmentContainerView,fragment)
+        fragTransaction.commit()
+    }
+}
---
+++ app/src/main/java/com/example/veterant2t/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.veterant2t
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    fun goNextScene(view: View){
+        val intent= Intent(this, HomeScreen::class.java)
+        startActivity(intent)
+    }
+}
