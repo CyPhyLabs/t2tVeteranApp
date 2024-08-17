1153a2ee33e03ec66641c42d7c045e049cd3d370
poseidonyash <129468436+poseidonyash@users.noreply.github.com>
2024-08-17 00:44:18 -0500

    HomeScreen Progress

--- app/src/main/java/com/example/veterant2t/HomeFragment.kt
+++ app/src/main/java/com/example/veterant2t/HomeFragment.kt
@@ -5,21 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.LinearLayout
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layout: LinearLayout = view.findViewById(R.id.firstNotificationLayout)
+        val layout2:LinearLayout = view.findViewById(R.id.secondNotificationLayout)
+        layout2.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                (activity as HomeScreen).binding.navView.selectedItemId = R.id.navigation_notifications
+            }
+        })
+
+        layout.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                (activity as HomeScreen).binding.navView.selectedItemId = R.id.navigation_notifications
+            }
+        })
+    }
+
+    fun onNotificationClick(view: View) {
+        val fragManager = parentFragmentManager
+        val fragTransaction = fragManager.beginTransaction()
+        fragTransaction.replace(R.id.fragmentContainerView, NotifFragment())
+        fragTransaction.commit()
+    }
 }
