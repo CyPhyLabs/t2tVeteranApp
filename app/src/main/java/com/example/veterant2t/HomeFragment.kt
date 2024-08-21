bf646c5f2d9a278f976c28775de2e222644a80ca
poseidonyash <129468436+poseidonyash@users.noreply.github.com>
2024-08-21 11:29:14 -0500

    updated notification card

--- app/src/main/java/com/example/veterant2t/HomeFragment.kt
+++ app/src/main/java/com/example/veterant2t/HomeFragment.kt
@@ -13,26 +13,22 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val layout: LinearLayout = view.findViewById(R.id.firstNotificationLayout)
-        val layout2:LinearLayout = view.findViewById(R.id.secondNotificationLayout)
-        layout2.setOnClickListener(object:View.OnClickListener{
-            override fun onClick(v: View?) {
-                (activity as HomeScreen).binding.navView.selectedItemId = R.id.navigation_notifications
-            }
-        })
+        val messageLinearLayout: LinearLayout = view.findViewById(R.id.messageLinearLayout)
+        val messageLinearLayout2: LinearLayout = view.findViewById(R.id.messageLinearLayout2)
 
-        layout.setOnClickListener(object:View.OnClickListener{
-            override fun onClick(v: View?) {
-                (activity as HomeScreen).binding.navView.selectedItemId = R.id.navigation_notifications
-            }
-        })
+        messageLinearLayout2.setOnClickListener {
+            (activity as HomeScreen).binding.navView.selectedItemId = R.id.navigation_notifications
+        }
+
+        messageLinearLayout.setOnClickListener {
+            (activity as HomeScreen).binding.navView.selectedItemId = R.id.navigation_notifications
+        }
     }
 
     fun onNotificationClick(view: View) {
